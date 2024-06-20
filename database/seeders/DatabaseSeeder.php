49f2cbd472595b03a726e9c4c1356a2bf7ae5407
Alansno <techoro1230@gmail.com>
2024-06-20 15:48:11 -0600

    usuarios admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,36 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'alan.admin@gmail.com',
+            'role' => 'Admin',
+            'password' => Hash::make('AdminAlan1230')
+        ]);
+
+        User::factory()->create([
+            'email' => 'carlos.admin@gmail.com',
+            'role' => 'Admin',
+            'password' => Hash::make('AdminCarlos1230')
+        ]);
+
+        User::factory()->create([
+            'email' => 'julio.admin@gmail.com',
+            'role' => 'Admin',
+            'password' => Hash::make('AdminJulio1230')
+        ]);
+
+        Admin::factory()->create([
+            'name' => 'Alan Snow',
+            'user_id' => 1
+        ]);
+
+        Admin::factory()->create([
+            'name' => 'Carlos MT',
+            'user_id' => 2
+        ]);
+
+        Admin::factory()->create([
+            'name' => 'Yuyo',
+            'user_id' => 3
         ]);
     }
 }
